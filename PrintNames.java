8249621eec3133c047c5956559195143565413b6
muidstim <tim.gir@mahidol.edu>
2017-01-09 08:16:12 +0700

    Update PrintNames.java

--- PrintNames.java
+++ PrintNames.java
@@ -5,7 +5,7 @@ public class PrintNames {
      *
      */
     public static void printYourNames() {
-         System.out.println("BBBBBBb");
+         System.out.println("BccccccBBBBBb");
          System.out.println("Bat");
          System.out.println("Jean");
          System.out.println("Lily");

af8d71fa5d0dc165fad028f5cf9ec3bee680ef8c
student36 <student36>
2017-01-17 14:10:08 +0700

    noob Pun!

--- src/com/muids/Titanium.java
+++ src/com/muids/Titanium.java
@@ -1,7 +1,41 @@
 package com.muids;
 
+import twitter4j.Status;
+import twitter4j.Twitter;
+import twitter4j.TwitterFactory;
+
+import java.util.List;
+
 /**
  * Created by MUIDS on 1/17/2017.
  */
 public class Titanium {
+    public static void main(String args[]) throws Exception {
+        Titanium.go();
+    }
+
+    public static void go() throws Exception {
+
+        String user = "@aer_____";
+        System.out.println("Finding the latest tweet from " + user);
+        Status latestTweet = getLatestTweet(user);
+        System.out.println("Latest Tweet:");
+        System.out.println(latestTweet.getUser().getName() + ":" + latestTweet.getText());
+        System.out.println("Retweeting...");
+        retweet(latestTweet.getId());
+    }
+
+    public static Status getLatestTweet(String user) throws Exception{
+        Twitter twitter = TwitterFactory.getSingleton();
+        List<Status> statuses = twitter.getUserTimeline(user);
+
+        return statuses.get(0);
+
+    }
+
+    private static void retweet(long statusID) throws Exception {
+        Twitter twitter = TwitterFactory.getSingleton();
+        twitter.retweetStatus(statusID);
+    }
+
 }
